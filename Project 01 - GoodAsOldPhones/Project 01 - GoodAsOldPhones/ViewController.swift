062b4d63bc8b5d6bc07fa2eb085bdb952c9a9170
wangyanchao <ych.wang@outlook.com>
2017-03-28 22:51:19 +0800

    add Project 01 - GoodAsOldPhones

---
+++ Project 01 - GoodAsOldPhones/Project 01 - GoodAsOldPhones/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 01 - GoodAsOldPhones
+//
+//  Created by Durand on 28/3/17.
+//  Copyright © 2017年 com.Durand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
