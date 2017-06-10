cd150a2a849fb64f5d85f83db32ca3eb141a25e9
wangyanchao <ych.wang@outlook.com>
2017-06-10 23:38:41 +0800

    08-wipe

--- Project 08 - SimpleRSSReader/Project 08 - SimpleRSSReader/ViewController.swift
+++ Project 08 - SimpleRSSReader/Project 08 - SimpleRSSReader/ViewController.swift
@@ -12,7 +12,18 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let three = YCTestEnum.Three;
+        print("\(three)");
+        
+        let one = YCEnum.YCEnumOne(start: 1.0, end: 2.0);
+        switch one {
+        case let .YCEnumOne(s,e):
+            print("start = \(s), end = \(e)");
+        case let .YCEnumTwo(s,e):
+            print("start = \(s), end = \(e)");
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +32,30 @@ class ViewController: UITableViewController {
     }
 
 
+}
+
+enum YCEnum {
+    case YCEnumOne(start: Double, end: Double);
+    case YCEnumTwo(start: Double, end: Double);
+}
+
+enum YCTestEnum: Int {
+    
+    case One = 1;
+    case Two = 2;
+    case Three, Four, Five
+    func testEnum() -> String {
+        switch self {
+        case .Three:
+            return "Three11";
+        case .Four:
+            return "Three22";
+        case .Five:
+            return "Three33";
+        default:
+            return String(self.rawValue);
+        }
+    }
+    
 }
 
