fc9f06fff7b3da6ec1619110098ddb551a42eac2
阿超 <ych.wang@outlook.com>
2017-06-15 23:24:07 +0800

    [old]-[14] init

---
+++ 30DaysofSwift/Project 14 - EmojiSlotMachine/Project 14 - EmojiSlotMachine/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Project 14 - EmojiSlotMachine
+//
+//  Created by Durand on 2017/6/15.
+//  Copyright © 2017年 Durand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var emojiPickerView: UIPickerView!
+    @IBOutlet weak var goButton: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
